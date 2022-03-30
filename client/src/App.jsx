6db01ec1f2be49954320fafb225d5afa44ec1eac
ahmedhanora11 <ahmedhanora11@gmail.com>
2022-03-31 07:29:28 +0800

    initializing the structure version 1.0

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+/* Important imports  */
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+/* Importing components */
+import ChannelContainer from './components/ChannelContainer';
+import ChannelListContainer from './components/ChannelListContainer';
+
+/* API key implementation from StreamAPI */
+const apiKey = '793eydgeka9b';
+
+/* instance */
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className='app__wrapper'>
+        <h1>Chatoo</h1>
+        <Chat client={client} theme="team light"> {/* Channel container & Channel container list*/}
+            <ChannelContiner/>
+            <ChannelListContiner/>
+        </Chat>
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* Comment for importing the app */
+import App from './App';
+/* rendering the app using the root, simply connecting the index*/
+ReactDOM.render(<App />, document.getElementById('root'));
