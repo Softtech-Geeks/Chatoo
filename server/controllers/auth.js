59c8083b9e34aec3050ad4a807343a94611f867f
OsamaAbdelhameed <osamaabdelhameed41@gmail.com>
2022-04-20 18:15:51 +0200

    Authorization 3.0v

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,8 @@
+const { consect } = require('getstream')
+const bcrypt = require('bcrypt')
+const StreamChat = require('stream-chat')
+const crypto = require('crypto')
+
 const login = (req, res) => {
     try {
 
@@ -8,9 +13,16 @@ const login = (req, res) => {
     }
 }
 
+const api_key = process.env.STREAM_API_KEY
+const api_secret = process.env.STREAM_API_SECRET
+const api_id = process.env.STREAM_API_ID
+
 const signup = (req, res) => {
     try {
         const { fullName, userName, phone, password, confirm, avatarURL, img } = req.body;
+        const userId = crypto.randomBytes(16).toString('hex')
+        const serverClient = connect(api_key, api_secret, api_id)
+
     } catch (error) {
         console.log(error)
 
