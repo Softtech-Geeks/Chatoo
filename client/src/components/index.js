0086808178ac684dbeef02fa38ceb220d3713ff7
JiyoonSong <36833776+JiyoonSong@users.noreply.github.com>
2022-05-05 12:15:25 +0900

    Fixed 2 errors in code, but still remaining the problem of sign up

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,6 +4,7 @@ export { default as ChannelSearch } from './ChannelSearch';
 export { default as TeamChannelList } from './TeamChannelList';
 export { default as TeamChannelPreview } from './TeamChannelPreview';
 export { default as Auth } from './Auth';
+export { default as UserList } from './UserList';
 export { default as ChannelInner } from './ChannelInner';
 export { default as CreateChannel } from './CreateChannel';
 export { default as EditChannel } from './EditChannel';
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 // import libraries and functions from StreamAPI and bcrypt to encrypt password
-const { consect } = require('getstream');
+const { connect } = require('getstream');
 const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
@@ -7,9 +7,25 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 // import enviroment variables from .env file
-const api_key = process.env.STREAM_API_KEY;
+const api_key = process.env.STREAM_API_KEY; 
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
+
+// signup function
+const signup = async(req, res) => {
+    try {
+        const { fullName, userName, password, phone} = req.body;
+        const userId = crypto.randomBytes(16).toString('hex');
+        const serverClient = connect(api_key, api_secret, app_id);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const token = serverClient.createUserToken(userId);
+        res.status(200).json({ token, fullName, userName, userId, hashedPassword, phone });
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ message: error });
+    }
+}
 
 // login function
 const login = async(req, res) => {
@@ -28,22 +44,6 @@ const login = async(req, res) => {
         } else {
             res.status(500).json({ message: 'Incorrect password' });
         }
-    } catch (error) {
-        console.log(error);
-
-        res.status(500).json({ message: error });
-    }
-}
-
-// signup function
-const signup = async(req, res) => {
-    try {
-        const { fullName, userName, phone, password, confirm, avatarURL, img } = req.body;
-        const userId = crypto.randomBytes(16).toString('hex');
-        const serverClient = connect(api_key, api_secret, app_id);
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const token = serverClient.createUserToken(userId);
-        res.status(200).json({ token, fullName, userName, userId, hashedPassword, phone });
     } catch (error) {
         console.log(error);
 
