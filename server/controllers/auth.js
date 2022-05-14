fbde874736c4b7e78d7d4e1d475c50a3980ffc23
ahmedhanora11 <ahmedhanora11@gmail.com>
2022-05-15 04:32:58 +0800

    login btn & some fixes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,7 +21,7 @@ const login = async(req, res) => {
 
         const token = serverClient.createUserToken(users[0].id);
         if (success) {
-            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id });
+            res.status(200).json({ token, fullName: users[0].fullName, userName, userId: users[0].id });
         } else {
             res.status(500).json({ message: 'Incorrect password' });
         }
