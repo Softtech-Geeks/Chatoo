19b805283b6d02bf34934b41c0369746fbfaf58a
ahmedhanora11 <ahmedhanora11@gmail.com>
2022-05-28 10:47:47 +0800

    Website is ready and online

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -36,7 +36,7 @@ const Auth = () => {
 		
 		const {userName, password, phone, avatarURL} = form;
 
-		const URL = 'http://localhost:5000/auth';
+		const URL = 'https://chatoo-sg.herokuapp.com/auth';
 		
 		const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             userName, password, fullName: form.fullName, phone, avatarURL,
